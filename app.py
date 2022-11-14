8783355fd17dfdc5645a67411721812f2c225f5a
hammonds185 <hammonds185@osu.edu>
2022-11-14 16:30:40 -0500

    up next use textbelt

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template, request
-
+import tweepy
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+    
 	return render_template('home.html')
 
 if __name__=='__main__':
